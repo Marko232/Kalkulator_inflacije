e5248754d36b453d4e7e87da4a1c211dc110d1c2
Marko <marko.eres232@gmail.com>
2024-03-11 13:23:07 +0100

    Drugi commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function inflationCalculator(){
+    let inflationRate = document.querySelector('#inflationRate');
+    let money = document.querySelector('#money');
+
+    // parseFloat sluzi za pretvaranje Stringa u broj sa decimalama.
+    inflationRate = parseFloat(inflationRate.value);
+    money = parseFloat(money.value);
+
+    let years = document.querySelector('#years').value;
+    years=parseFloat(years);
+
+    //Fromula za izracunavanje inflacije.
+
+    let worth = money + (money* (inflationRate / 100));
+
+        for(let i=1;i < years; i++) {
+            worth += worth * (inflationRate / 100);
+        }
+
+        let newElement = document.createElement('div');
+        newElement.className = 'new-Value';
+        newElement.innerText = `Danasnjih ${money}€ vrijedi isto kao ${worth}€ za ${years} godina.`;
+
+        document.querySelector('.container').appendChild((newElement));
+}
